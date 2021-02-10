51c8d980e69641c5ed2a0d5bbd719c209a9bc7bf
ustinovich.yuryi <ustinhtc@gmail.com>
2021-02-10 16:23:07 +0300

    connected asp to mssql in container is exposed on port 1433

--- Data/Migrations/20210210124153_turn db to container v1.Designer.cs
+++ Data/Migrations/20210210124153_turn db to container v1.Designer.cs
@@ -4,14 +4,16 @@ using ApiBankBackBone.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ApiBankBackBone.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210210124153_turn db to container v1")]
+    partial class turndbtocontainerv1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
